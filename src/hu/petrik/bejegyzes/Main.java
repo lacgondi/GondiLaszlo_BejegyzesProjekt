bd72b4c843e2a8b106f2b7eed225d768fed3379c
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-29 08:44:03 +0200

    Add test for like function

--- src/hu/petrik/bejegyzes/Main.java
+++ src/hu/petrik/bejegyzes/Main.java
@@ -32,10 +32,15 @@ public class Main {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
         while (scR.hasNextLine()){
             String[] temp = scR.nextLine().split(";");
             bejegyzesList.add(new Bejegyzes(temp[0],temp[1]));
+        }
+
+        for (int i = 0; i < bejegyzesList.size()*20; i++) {
+            if ((Math.random()*1)==1){
+                bejegyzesList.get(i).like();
+            }
         }
     }
 }
