8f7bee0df44ce02606445a800247a2614ca73c58
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-29 08:33:14 +0200

    Add list to Main.java
    The program also takes input from the user and throws error if the input is incorrect

--- src/hu/petrik/bejegyzes/Main.java
+++ src/hu/petrik/bejegyzes/Main.java
@@ -1,7 +1,26 @@
 package hu.petrik.bejegyzes;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        List<Bejegyzes> bejegyzesList = new ArrayList<>();
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Mennyi bejegyzést szeretne hozzáadni a listához: ");
+        int inNum = 0;
+        try {
+            inNum=sc.nextInt();
+        }catch (InputMismatchException e){
+            System.out.println(e);
+            return;
+        }
+
+        for (int i = 0; i < inNum; i++) {
+            bejegyzesList.add(new Bejegyzes("author"+i, "content"+i));
+        }
     }
 }
