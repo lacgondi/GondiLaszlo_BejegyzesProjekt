cc056730fffbfa3c005ea07d74deed36c06f57fe
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-29 08:39:25 +0200

    Make read from file functionality

--- src/hu/petrik/bejegyzes/Main.java
+++ src/hu/petrik/bejegyzes/Main.java
@@ -1,5 +1,7 @@
 package hu.petrik.bejegyzes;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -21,6 +23,19 @@ public class Main {
 
         for (int i = 0; i < inNum; i++) {
             bejegyzesList.add(new Bejegyzes("author"+i, "content"+i));
+        }
+
+        File fileName = new File("bejegyzesek.csv");
+        Scanner scR = null;
+        try {
+            scR=new Scanner(fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (scR.hasNextLine()){
+            String[] temp = scR.nextLine().split(";");
+            bejegyzesList.add(new Bejegyzes(temp[0],temp[1]));
         }
     }
 }
