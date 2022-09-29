7f4cb1da37f967319719dd4e7763abab9e275453
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-29 09:40:39 +0200

    Change list order

--- src/hu/petrik/bejegyzes/Main.java
+++ src/hu/petrik/bejegyzes/Main.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static <T> void main(String[] args) {
         List<Bejegyzes> bejegyzesList = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
